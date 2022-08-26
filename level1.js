84d22a76aaf57ad578e391c91a596833c6d6a4c8
Swateelagna Sahoo <swateelagnasahoo@gmail.com>
2022-08-26 23:09:14 +0530

    first commit

---
+++ level1.js
@@ -0,0 +1,11 @@
+//q1
+let firstName = "Swatee";
+lastName = "S";
+country = "india";
+city = "BBSR";
+age = 25;
+isMarried = false;
+const now = new Date();
+const year = now.getFullYear();
+console.log(firstName, lastName, country, city, age, isMarried, year);
+//q2
