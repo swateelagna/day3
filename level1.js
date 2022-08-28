331539d0b15b245955b4a07f9ec0191e2961971d
Swateelagna Sahoo <swateelagnasahoo@gmail.com>
2022-08-28 23:14:41 +0530

    completes q5 i of level1

--- level1.js
+++ level1.js
@@ -17,5 +17,27 @@ let num1=5
 let num2=5
 console.log(num1==num2)
 //i
+let stringT1='This string is truthy'
+console.log(stringT1)
+const int=50
+console.log(int)
+let isOccupied=true
+console.log(isOccupied)
+//ii
+let stringF=''
+console.log(stringF)
+let numF=0
+console.log(numF)
+let isStudying=false 
+console.log(isStudying)
+//q5
+//i
+let a=4
+b=3
+console.log("4 is greater than 3")
+console.log(4>3)
+//ii
+
+
 
 
