0f1ae2c86d22d968c6d3e7f2003dce3a7a7b89cb
Swateelagna Sahoo <swateelagnasahoo@gmail.com>
2022-09-02 13:47:57 +0530

    completes q2 of level2

--- level2.js
+++ level2.js
@@ -5,4 +5,8 @@ const areaOfTriangle = 0.5 * b * h;
 console.log("Enter base: b");
 console.log("Enter height: h");
 console.log(areaOfTriangle);
-
+//2
+let a = +prompt("Enter side a:");
+let d = +prompt("Enter side d:");
+let c = +prompt("Enter side c:");
+console.log(`Perimeter of Triangle is ${a + d + c}`);
