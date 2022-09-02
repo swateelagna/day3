5eb466885e18255d4ba9fed7dc04f2ba1ab554c9
Swateelagna Sahoo <swateelagnasahoo@gmail.com>
2022-09-02 12:20:02 +0530

    completes q5

--- level1.js
+++ level1.js
@@ -9,35 +9,66 @@ const now = new Date();
 const year = now.getFullYear();
 console.log(firstName, lastName, country, city, age, isMarried, year);
 //q2
-console.log(typeof '10')
+console.log(typeof "10");
 //q3
-console.log(parseInt('9.8'))
+console.log(parseInt("9.8"));
 //q4
-let num1=5
-let num2=5
-console.log(num1==num2)
+let num1 = 5;
+let num2 = 5;
+console.log(num1 == num2);
 //i
-let stringT1='This string is truthy'
-console.log(stringT1)
-const int=50
-console.log(int)
-let isOccupied=true
-console.log(isOccupied)
+let stringT1 = "This string is truthy";
+console.log(stringT1);
+const int = 50;
+console.log(int);
+let isOccupied = true;
+console.log(isOccupied);
 //ii
-let stringF=''
-console.log(stringF)
-let numF=0
-console.log(numF)
-let isStudying=false 
-console.log(isStudying)
+let stringF = "";
+console.log(stringF);
+let numF = 0;
+console.log(numF);
+let isStudying = false;
+console.log(isStudying);
 //q5
 //i
-let a=4
-b=3
-console.log("4 is greater than 3")
-console.log(4>3)
+let a = 4;
+b = 3;
+console.log("4 is greater than 3");
+console.log(a > b);
 //ii
-
-
-
-
+console.log("4 is greater than equal to 3");
+console.log(a >= 3);
+//iii
+console.log("4 is less than 3 which is false");
+console.log(a < b);
+//iv
+console.log("4 is less than equal to 3 which is false");
+console.log(a <= b);
+//v
+let c = 4;
+let d = 4;
+console.log("4 is equal to 4");
+console.log(c == d);
+//vi
+console.log("4 === 4");
+console.log(c === d);
+//vii
+console.log("4 is not equal to 4 which is false");
+console.log(c != d);
+//viii
+console.log("4 !== 4 which is false");
+console.log(c !== d);
+//ix
+let e = 4;
+let f = "4";
+console.log("4 is not equal to '4' which is false");
+console.log(e != f);
+//x
+console.log("4=='4' which is true");
+console.log(e == f);
+//xi
+console.log("e === f is false");
+console.log(e === f);
+//xii
+console.log("python".length > "jargon".length);
